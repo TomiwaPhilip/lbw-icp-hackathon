ea9ce399c87e50c83773c9deb9d3897c018927a0
Tomiwa Philip <110630370+TomiwaPhilip@users.noreply.github.com>
2024-07-03 16:03:04 +0000

    updated images

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,32 +1,38 @@
 "use client";
 
+import Image from "next/image";
 import Link from "next/link";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
     <>
-      <div className="flex justify-around h-screen">
-        <div className="w-[50%] flex flex-col gap-4 justify-center">
+      <div className="flex flex-col lg:flex-row justify-center items-center h-screen gap-10 p-5 lg:p-0">
+        <div className="w-full lg:w-1/2 flex flex-col gap-4 justify-center">
           <div className="px-5">
-            <h1 className="text-center">
-              <span className="block text-[2.5rem] font-bold text-left">Decentralized Energy Payment System</span>
+            <h1 className="text-center lg:text-left">
+              <span className="block text-[2rem] lg:text-[2.5rem] font-bold">Decentralized Energy Payment System</span>
             </h1>
             <p>
               This system allows energy producers, such as homeowners with solar panels, to sell excess energy directly
               to consumers. By using smart contracts, transactions are automated, transparent, and secure, ensuring that
               payments are made instantly once energy is delivered. This decentralized approach enhances efficiency,
               reduces costs, and promotes the use of renewable energy sources by providing a flexible and accessible
-              marketplace for energy trading
+              marketplace for energy trading.
             </p>
           </div>
-          <div className="px-5 ">
-            <Link href="/buy-energy" className="py-3 px-6 bg-primary rounded-[10px]">
+          <div className="px-5">
+            <Link
+              href="/buy-energy"
+              className="py-3 px-6 block text-center bg-[#00247b] rounded-lg text-white btn-ghost dark:bg-white dark:text-black"
+            >
               Get Started
             </Link>
           </div>
         </div>
-        <div className="w-[30%]"></div>
+        <div className="w-full lg:w-1/3 mt-10 lg:mt-0 flex justify-center lg:justify-start">
+          <Image src="/meter.png" alt="meter" width={400} height={400} />
+        </div>
       </div>
     </>
   );
